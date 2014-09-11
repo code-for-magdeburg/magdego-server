5d048f23aacdfce10ce624f65017d0d067c7201b
Rosario Raulin <post@rosario-raulin.de>
2014-09-11 15:13:34 +0200

    first complete version

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ var bodyParser = require('body-parser')
 var http = require('http');
 
 var departure = require('./departure');
+var async = require('async');
 
 // connect to mongo db
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://' + cfg.mongodb_host + '/' + cfg.mongodb_name);
+var connection_string = 'mongodb://' + cfg.mongodb_host + '/' + cfg.mongodb_database;
+// console.log(connection_string);
+mongoose.connect(connection_string);
 var db = mongoose.connection;
 
 db.on('error', function (err) {
@@ -35,59 +38,92 @@ app.use(bodyParser.json());
 
 var router = express.Router();
 
-router.get('/:id', function (req, res) {
-  Station.findOne({id: req.params.id}, function (err, station) {
+router.get('/stations', function (req, res) {
+  Station.find({}, function (err, stations) {
     if (err) {
-      res.status(500).json({error: 'internal error'});
+      res.status(500).json({error: 'couldn\'d find stations'});
+      res.end();
     } else {
-      if (station === null) {
-        res.status(404).json({error: 'station not found'});
-      } else {
-        res.json(station);
-      }
-    }
-    res.end();
-  });
-});
-
-router.get('/find/:long/:lat', function (req, res) {
-  var lat = parseFloat(req.params.lat);
-  var long = parseFloat(req.params.long);
-
-  var nearPoint = {type: "Point", coordinates: [long, lat] };
-  console.log(nearPoint);
-
-  Station.aggregate(
-    [{ $geoNear: {
-      near: nearPoint,
-      distanceField: "dist.calculated",
-      includeLocs: "dist.location",
-      num: 5,
-      spherical: true
-    }
-  }], function (err, result) {
-    if (err) {
-      console.log("error aggregating: " + err.stack);
-    } else {
-      var deptimes = [];
-
-      result.forEach(function (val, index, array) {
-        departure.get_departure_times(function (error, result) {
-          if (error) {
-            // TODO: error handling...
-          } else {
-            deptimes[index] = result;
-          }
-        });
-      });
-
-      res.json(deptimes);
+      res.json(stations);
       res.end();
     }
   });
 });
 
-app.use('/station', router);
+router.get('/departure-time/station/:id', function (req, res) {
+  Station.findOne({id: req.params.id}, function (err, station) {
+    if (err) {
+      res.status(500).json({error: 'internal error'});
+      res.end();
+    } else {
+      if (station === null) {
+        res.status(404).json({error: 'station not found'});
+        res.end();
+      } else {
+        departure.get_departure_times(req.params.id, function (err, result) {
+          if (err) {
+            res.status(500).json({error: "coudn't get departure times"});
+            res.end();
+          } else {
+            res.json(result);
+            res.end();
+          }
+        });
+      }
+    }
+  });
+});
+
+router.get('/departure-time/location/:long/:lat', function (req, res) {
+  var lat = parseFloat(req.params.lat);
+  var long = parseFloat(req.params.long);
+
+  var nearPoint = {type: "Point", coordinates: [long, lat] };
+
+  // query departure times from MVB
+  var departureMapper = function (station, callback) {
+    departure.get_departure_times(station.id, function (err, result) {
+      // console.log(station.name + ": " + station.dist.calculated + " (" + station.location.coordinates + ")");
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, { station_info: station, departure_times: result });
+      }
+    });
+  }
+
+  // mapping stations to departure times
+  var stationMapper = function (err, stations) {
+    if (err) {
+      res.status(500).json({error: "finding close stations failed"});
+      res.end();
+    } else {
+      async.map(stations, departureMapper, function (err, departureTimes) {
+        if (err) {
+          res.status(500).json({error: "getting departure times failed"});
+          res.end();
+        } else {
+          res.json(departureTimes);
+          res.end();
+        }
+      })
+    }
+  }
+
+  Station.aggregate([
+    {
+      $geoNear: {
+        near: nearPoint,
+        distanceField: "dist.calculated",
+        includeLocs: "dist.location",
+        num: 5,
+        spherical: true
+      }
+    }
+  ], stationMapper);
+});
+
+app.use('/', router);
 
 // module interface
 var server = http.createServer(app);
