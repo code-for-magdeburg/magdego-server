674ce047113260009a9c4ecfe6520f3d7f1674c3
Moezalez <moritzsattler91@gmail.com>
2015-02-03 20:12:06 +0100

    #1 added constants to own file

---
+++ constants.js
@@ -0,0 +1,5 @@
+module.exports = {
+  QUERY_STATIONS_PATH_BASE: 'http://reiseauskunft.insa.de/bin/query.exe/dny?performLocating=2&tpl=stop2json&look_maxno=20',
+  QUERY_JOURNEYS_PATH_BASE: 'http://reiseauskunft.insa.de/bin/stboard.exe/dn?L=.vs_stb&L=.vs_stb.vs_stb&boardType=dep&selectDate=today&productsFilter=0000011111&additionalTime=0&start=yes&requestType=0&outputMode=undefined&maxJourneys=30',
+
+};
